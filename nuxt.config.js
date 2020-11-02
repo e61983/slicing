a68f43f944cfc9e8e7120ce78818d203ec274c57
Yuan <e61983@gmail.com>
2020-11-03 01:13:50 +0800

    Workround for safari loading

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,10 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
+    // workround for fix safari loading issue
+    filenames: {
+      app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+      chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js'
+    }
+  },
 }
