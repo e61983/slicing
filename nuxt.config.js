101f3d2645454ae0d0528a340b47eb351010e436
Yuan <e61983@gmail.com>
2020-11-03 23:38:19 +0800

    Add font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,12 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    'nuxt-fontawesome'
   ],
+
+  fontawesome:{
+    component: 'fa'
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
